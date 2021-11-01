b48a59cffb21d04c71d61ca97a3a590868225b3e
akshaybhalotia <github@axy.one>
2021-11-01 14:28:20 +0530

    feat: project setup

---
+++ Dyte Swift Sample/Dyte Swift Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dyte Swift Sample
+//
+//  Created by Akshay Bhalotia on 01/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
